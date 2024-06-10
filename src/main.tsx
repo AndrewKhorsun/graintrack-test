23e868e8337e75c897fdd671ad12cdeb93fa4386
Andrii Khorsun <andrii.khorsun@cruxlab.com>
2024-06-10 20:16:16 +0300

    added react router, auth0, tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-fqrkzq1qu3nyp466.us.auth0.com"
+      clientId="SGNuBdx86HFENgh54rGoVYxRAMO2CjQb"
+      authorizationParams={{
+        redirect_uri: "http://localhost:5173",
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
-)
+);
